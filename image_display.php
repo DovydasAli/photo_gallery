0bd673383ee73d7adbaa3f8955e464500ca927df
DovydasAli <dovydas.ali@gmail.com>
2021-11-21 17:37:43 +0200

    Updated html looks a bit better.

--- image_display.php
+++ image_display.php
@@ -3,8 +3,9 @@
 
     $img = mysqli_query($db, "SELECT * FROM photo_gallery.photos WHERE user_id = '". $_SESSION['user_id']['id'] ."'"); // selects all images from database
     while ($row = mysqli_fetch_array($img)) {
-
-        echo "<img src='" . $row['image'] . "' height='200' width='200' >"; // displays the images form the query on the website
+        echo "<div class=col-lg-3 col-md-4 col-xs-6 thumb>
+                    <img src='" . $row['image'] . "' class=img-thumbnail>
+                </div>"; // displays the images form the query on the website
     }
 
 ?>
--- index.php
+++ index.php
@@ -72,12 +72,21 @@
                     </form>
                 </div>
             </section>
-        <?php include("image_display.php") ?>
+            <div class="container">
+                <div class="row">
+                    <div class="row">
+                        <?php include("image_display.php") ?>
+                    </div>
+                </div>
+            </div>
+
+
 
     <?php endif ?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
     </body>
 </html>
